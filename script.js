7c56c337ee5ba851b3533b9af19b3608e5b2dbad
Justin Fulton <jusfulton@gmail.com>
2020-08-19 18:12:26 -0700

    filter functionality added

--- script.js
+++ script.js
@@ -60,14 +60,38 @@ function showLoading() {
   }, 300);
 }
 
+
+//Filter Posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase()
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  })
+}
+
 //Show initial posts
 
 showPosts();
 
 window.addEventListener("scroll", () => {
-  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const {
+    scrollTop,
+    scrollHeight,
+    clientHeight
+  } = document.documentElement;
 
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     showLoading();
   }
 });
+
+filter.addEventListener('input', filterPosts)
